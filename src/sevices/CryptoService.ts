a3a41f8bbb2ebec5c8d4fbe7433b05d8ac217ae6
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-17 11:44:25 -0600

    Cleaning the store and importing the extra function

---
+++ src/sevices/CryptoService.ts
@@ -0,0 +1,16 @@
+import { CryptoCurrenciesSchema } from "../schema/cripto-schema";
+import axios from "axios";
+
+export async function getCryptos() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+
+  const {
+    data: { Data },
+  } = await axios(url);
+  //console.log(Date);
+  const result = CryptoCurrenciesSchema.safeParse(Data);
+  if (result.success) {
+    return result.data;
+  }
+}
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,12 @@
 import { create } from "zustand";
-import axios from "axios";
-import { CryptoCurrenciesSchema } from "../schema/cripto-schema";
 import { CryptoCurrency } from "../types";
 import { devtools } from "zustand/middleware";
+import { getCryptos } from "../sevices/CryptoService";
 
 type CryptoStore = {
   cryptoCurrencies: CryptoCurrency[];
   fetchCryptos: () => Promise<void>;
 };
-
-async function getCryptos() {
-  const url =
-    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
-
-  const {
-    data: { Data },
-  } = await axios(url);
-  //console.log(Date);
-  const result = CryptoCurrenciesSchema.safeParse(Data);
-  if (result.success) {
-    return result.data;
-  }
-}
 
 export const useCryptoStore = create<CryptoStore>()(
   devtools((set) => ({
