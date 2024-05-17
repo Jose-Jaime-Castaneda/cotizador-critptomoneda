608ff894e8f5414a5fe283bb612f576755c5d8a2
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-17 10:35:05 -0600

    Creating types with zod for the response

--- src/schema/cripto-schema.ts
+++ src/schema/cripto-schema.ts
@@ -4,3 +4,12 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string(),
 });
+
+export const CryptoCurrencySchema = z.object({
+  CoinInfo: z.object({
+    FullName: z.string(),
+    Name: z.string(),
+  }),
+});
+
+export const CryptoCurrenciesSchema = z.array(CryptoCurrencySchema);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,22 @@
 import { create } from "zustand";
+import axios from "axios";
+import { CryptoCurrenciesSchema } from "../schema/cripto-schema";
+
+async function getCryptos() {
+  const url =
+    "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+
+  const {
+    data: { Data },
+  } = await axios(url);
+  const result = CryptoCurrenciesSchema.safeParse(Data);
+  console.log(result);
+
+  console.log(Data);
+}
 
 export const useCryptoSrore = create(() => ({
   fetchCryptos: () => {
-    console.log("Hola..");
+    getCryptos();
   },
 }));
